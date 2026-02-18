9528680e4b27823a65fa33e52ba128ff75f6a208
taylanot <taylanozgurturan@gmail.com>
2026-02-18 21:53:01 +0100

    batman

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file math.cpp
+ * @brief Simple math example.
+ */
+
+/**
+ * @brief Adds two numbers.
+ *
+ * @param a First number
+ * @param b Second number
+ * @return Sum of a and b
+ */
+int add(int a, int b)
+{
+  return a + b;
+}
+
+/**
+ * @brief Entry point.
+ */
+int main()
+{
+  int result = add(2, 3);
+  return 0;
+}
